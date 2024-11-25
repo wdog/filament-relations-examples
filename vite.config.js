8ac685589dc53e8507873bfc231c7a45e0cef958
wdog <wdog666@gmail.com>
2024-11-25 23:52:12 +0100

    base with auto refresh

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: [
+                ...refreshPaths,
+                "app/Http/Livewire/**/*", // Custom Livewire components
+                "app/Filament/*", // Filament Resources
+                "app/Filament/**/*", // Filament Resources
+                "app/Providers/Filament/**",
+                "app/Forms/Components/**",
+                "app/Livewire/**",
+                "app/Infolists/Components/**",
+                "app/Tables/Columns/**",
+                "app/Providers/**",
+                "public/css/hasnayeen/themes/**",
+            ],
         }),
     ],
-server: { host: '0.0.0.0', hmr: { host: 'localhost' } },
+    server: { host: "0.0.0.0", hmr: { host: "localhost" } },
 });
